f9539ded86247c1a9d44d4efe3670ea7eaa555e2
Minhnguyet <minhnguyetpasal@gmail.com>
2019-10-22 20:45:07 +0700

    Chapter10

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: Settings.max50}
   validates :email, presence: true, length: {maximum: Settings.max255},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: true
-  validates :password, presence: true, length: {minimum: Settings.min6}
+  validates :password, presence: true, length: {minimum: Settings.min6}, allow_nil: true
 
   has_secure_password
 
